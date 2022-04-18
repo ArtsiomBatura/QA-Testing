8b08ed87f84ea4b4ee90c770ca3c65db4d1c3f1c
ArtsiomBatura <73168554+ArtsiomBatura@users.noreply.github.com>
2022-04-18 22:58:12 +0300

    Add files via upload

---
+++ AgesVaidation.js
@@ -0,0 +1,22 @@
+function checkAge(age) {
+    if (age < 18 ){
+    
+        return("You don`t have access cause your age is " + age + " It`s less then " + 18)
+    } 
+    else if  (age >= 18 && age <= 60 ){
+    
+        return("Welcome!")
+    }
+    else if  (age > 60){
+        return("Keep clam and look Culture channel");
+    }
+    else {
+        return("Technical work");
+    }
+  }
+  
+  var output = checkAge(12);
+  console.log(output); 
+
+
+  
